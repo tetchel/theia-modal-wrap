eefe921f9c2d2c820bc712f8348efcf6b82a387f
Tim Etchells <timetchells@ibm.com>
2020-06-23 11:17:08 -0400

    init
    Signed-off-by: Tim Etchells <timetchells@ibm.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# Test extension for Theia modal wrapping issue
+
+https://github.com/eclipse-theia/theia/issues/8071
+
+Just run the 'Hello world modal' command
+
+Copied from https://github.com/microsoft/vscode-extension-samples/tree/master/helloworld-sample
---
+++ src/extension.ts
@@ -0,0 +1,21 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "helloworld-sample" is now active!');
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('extension.helloWorldModal', () => {
+			vscode.window.showInformationMessage(`Here's a long message with some newlines in it. This message is intended to stretch the modal dialog so I can test wrapping. So that's why this message is so long.` +
+				`\n\nThis should be on a new line. This is another long line but it should be on a new line because there are two newline characters at the start of this line. They should render in the browser.` +
+				`\nThis should also be on a new line. There is a single newline at the start of this line.`,
+				{ modal: true }
+			);
+		})
+	);
+}
